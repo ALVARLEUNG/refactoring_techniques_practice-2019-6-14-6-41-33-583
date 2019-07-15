6df5523df0740f7fc9939b835d5273d681096bab
ALVARLEUNG <alvar.leung@qq.com>
2019-07-15 17:46:20 +0800

    test: add the test for  renderBanner
    should_return_banner_when_call_renderBanner_given_IE_and_chrome

--- src/test/java/com/tws/refactoring/extract_variable/BannerRenderTest.java
+++ src/test/java/com/tws/refactoring/extract_variable/BannerRenderTest.java
@@ -3,15 +3,20 @@ package com.tws.refactoring.extract_variable;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class BannerRenderTest {
 
     @Test
-    public void should_return_IE_on_Mac_when_call_renderBanner_given_IE_on_Mac () {
+    public void should_return_IE_or_Mac_when_call_renderBanner_given_IE_on_Mac () {
         BannerRender bannerRender = new BannerRender();
         String result = bannerRender.renderBanner("Mac", "IE");
         Assert.assertEquals("IE or Mac?",result);
+    }
+
+    @Test
+    public void should_return_banner_when_call_renderBanner_given_IE_and_chrome () {
+        BannerRender bannerRender = new BannerRender();
+        String result = bannerRender.renderBanner("IE", "chrome");
+        Assert.assertEquals("banner",result);
     }
 
 }
